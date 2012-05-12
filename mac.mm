deeccdc0f6f5e8e7a17d48c514fb176a3ce2aeee
Cameron Tinker <ctinke1@tigers.lsu.edu>
2012-05-11 22:15:58 -0500

    Now lists all application windows for foremost application. Use this to grab the window title from the various window you are interested in.

--- mac.mm
+++ mac.mm
@@ -1,34 +1,49 @@
 #include "mac.h"
+#include "Cocoa/Cocoa.h"
 
-#import "Cocoa/Cocoa.h"
 void Mac::getWindowTitle()
 {
-    int i =0;
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    NSMutableArray *windows = (NSMutableArray *)CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly | kCGWindowListExcludeDesktopElements, kCGNullWindowID);
+    // get front process for currently active application
+    ProcessSerialNumber psn = { 0L, 0L };
+    OSStatus err = GetFrontProcess(&psn);
 
-    for (NSDictionary *window in windows) {
-        NSString *owner = [window objectForKey:@"kCGWindowOwnerName" ];
-        NSString *name = [window objectForKey:@"kCGWindowName" ];
-        NSNumber *layer = [window objectForKey:@"kCGWindowLayer"];
+    CFStringRef processName = NULL;
+    err = CopyProcessName(&psn, &processName);
+    CFIndex length = CFStringGetLength(processName);
 
-        if([layer intValue] == 0 && i == 0){/* only returns the first window title.
-Window titles are returned are in order from front to back and i only want the
-frontmost active window title.*/
-            NSString *title =  [owner stringByAppendingString:@" - "];
-            title = [title stringByAppendingString:name];
+    QString string(length, Qt::Uninitialized);
+    CFStringGetCharacters(processName, CFRangeMake(0, length), reinterpret_cast<UniChar *>
+                          (const_cast<QChar *>(string.unicode())));
+    qDebug() << string;
+    NSString *pname = (NSString *)processName;
 
-            NSRange range;
-            range.location = 0;
-            range.length = [title length];
-//            QString result(range.length, QChar(0));
 
-            unichar *chars = new unichar[range.length];
-            [title getCharacters:chars range:range];
-            QString foregroundAppTitle = QString::fromUtf16(chars, range.length);
-            delete[] chars;
-            qDebug() << foregroundAppTitle;
-            i++;
+    // loop through all application windows
+    int i = 0;
+    CFArrayRef windowList = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly | kCGWindowListExcludeDesktopElements, kCGNullWindowID);
+    for (NSMutableDictionary* entry in (NSArray*)windowList)
+    {
+        NSString* ownerName = [entry objectForKey:(id)kCGWindowOwnerName];
+        NSString *name = [entry objectForKey:@"kCGWindowName" ];
+        NSInteger ownerPID = [[entry objectForKey:(id)kCGWindowOwnerPID] integerValue];
+        NSInteger layer = [[entry objectForKey:@"kCGWindowLayer"] integerValue];
+        if(layer == 0)
+        {
+            //if([ownerName isEqualToString:@"Firefox"] || [ownerName isEqualToString:@"Google Chrome"] || [ownerName isEqualToString:@"Safari"] )
+            if([ownerName isEqualToString:pname])
+                    NSLog(@"%@:%d name: %@, layer: %d, i: %d", ownerName, ownerPID, name, layer, i);
+                i++;
         }
     }
+    CFRelease(windowList);
+    CFRelease(processName);
+//            NSRange range;
+//            range.location = 0;
+//            range.length = [title length];
+////            QString result(range.length, QChar(0));
+
+//            unichar *chars = new unichar[range.length];
+//            [title getCharacters:chars range:range];
+//            QString foregroundAppTitle = QString::fromUtf16(chars, range.length);
+//            delete[] chars;
 }
